1987e7b6e1c8363ee7b2829de48f84743a206125
GabrielMastny <mastny.gabriel@gmail.com>
2020-03-30 19:48:51 +0200

    Login implementation + api request for my stations

---
+++ CTU60GLib/Client/CTUClient.cs
@@ -0,0 +1,105 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Client.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CTU60GLib.Client
+{
+    public class CTUClient
+    {
+        private HttpClient httpClient;
+        private Uri baseAddres = new Uri("https://60ghz.ctu.cz");
+        private string frontEndToken ="";
+
+        public CTUClient()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            };
+
+            httpClient = new HttpClient(handler);
+            httpClient.BaseAddress = baseAddres;
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+            httpClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
+            httpClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("cs", 0.9));
+            httpClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("en", 0.6));
+
+        }
+
+        public async Task LoginAsync(string login,string pass)
+        {
+            HttpResponseMessage response = await httpClient.GetAsync("prihlaseni");
+            if(response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                frontEndToken = await ObtainFrontEndToken(response.Content.ReadAsStreamAsync());
+            }
+            else
+            {
+                //todo
+                throw new NotImplementedException();
+            }
+
+            Dictionary<string, string> postContent = new Dictionary<string, string>()
+            {
+                {"_csrf-frontend",frontEndToken },
+                {"LoginForm[email]",login },
+                {"LoginForm[password]",pass },
+                {"login-button","" }
+            };
+
+            response = await httpClient.PostAsync("prihlaseni", new FormUrlEncodedContent(postContent));
+
+            if (response.StatusCode != HttpStatusCode.OK) { }//todo throw exception
+        }
+
+        private async Task<string> ObtainFrontEndToken(Task<Stream> stream)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(await stream);
+            HtmlNode mdnode = doc.DocumentNode.SelectNodes("//meta[@name='csrf-token']").FirstOrDefault();
+
+            if (mdnode != null || mdnode == default) { }//todo throw exception
+
+            return mdnode.Attributes["content"].Value;
+        }
+
+        public async Task<List<CtuWirelessUnit>> GetMyStationsAsync()
+        {
+            HttpResponseMessage response = await httpClient.GetAsync("/api/v1/station/my-stations");
+            try
+            {
+                var x = response.Content.ReadAsStringAsync().Result;
+
+                var jObj = JObject.Parse(x).Children().Children().ToList()[1].ToList();
+
+                List<CtuWirelessUnit> wirelessUnits = new List<CtuWirelessUnit>();
+
+                foreach (var item in jObj)
+                {
+                    wirelessUnits.Add(CtuWirelessUnit.FromJson(item.Children().FirstOrDefault().ToString()));
+                }
+
+                return wirelessUnits;
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+            
+        }
+
+
+    }
+}
---
+++ CTU60GLib/Client/Json/CTUJsonResponse.cs
@@ -0,0 +1,68 @@
+﻿namespace Client.Json
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class CtuWirelessUnit
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("lat")]
+        public string Lat { get; set; }
+
+        [JsonProperty("lng")]
+        public string Lng { get; set; }
+
+        [JsonProperty("id_station_pair")]
+        public string IdStationPair { get; set; }
+
+        [JsonProperty("id_master")]
+        public string IdMaster { get; set; }
+
+        [JsonProperty("id_m")]
+        public string IdM { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("id_user")]
+        public string IdUser { get; set; }
+
+        [JsonProperty("pair_position")]
+        public string PairPosition { get; set; }
+
+        [JsonProperty("typeName")]
+        public string TypeName { get; set; }
+    }
+
+    public partial class CtuWirelessUnit
+    {
+        public static CtuWirelessUnit FromJson(string json) => JsonConvert.DeserializeObject<CtuWirelessUnit>(json, Client.Json.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this CtuWirelessUnit self) => JsonConvert.SerializeObject(self, Client.Json.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
